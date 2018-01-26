be3b855d6767df2f111551b1a91cf57567ad7a1d
junglechen@163.com <>
2018-01-26 13:46:00 +0800

    Signed-off-by: junglechen@163.com <>

---
+++ src/main/java/cn/jungle/cloud/config/EurekaServer.java
@@ -0,0 +1,22 @@
+package cn.jungle.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(EurekaServer.class, args);
+        System.out.println( "Hello EurekaServer!" );
+    }
+}
