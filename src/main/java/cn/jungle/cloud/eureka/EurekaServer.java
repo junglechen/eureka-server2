f7b344a0a3b372afa9cc51402d2c39cae5808da5
junglechen@163.com <JUNGLE@DESKTOP-022C5Q4>
2018-08-15 11:03:50 +0800

    CHANGE2

---
+++ src/main/java/cn/jungle/cloud/eureka/EurekaServer.java
@@ -0,0 +1,22 @@
+package cn.jungle.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+/**
+ * Hello world!
+ *
+ */
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(EurekaServer.class, args);
+        System.out.println( "Hello EurekaServer 2!" );
+    }
+}
